671574b8c82c7fd6bb46d1c47c7810b01d85919a
bafraiki <bafraiki@student.42.fr>
2019-06-22 19:45:10 +0200

    going really fast on image, maybe I will be much slower on the server side

--- galerie/index.php
+++ galerie/index.php
@@ -1,8 +1,16 @@
 <?php
+session_start();
 require_once('../header/layout.php');
 layout("my gallerie");
 ?>
 
 
+		<video id="video"></video>
+		<button id="startbutton">Prendre une photo</button>
+		<canvas id="canvas"></canvas>
+    <img id="photo" alt="The screen capture will appear in this box.">
+
+
+		<script charset="utf-8" src="galerie/galerie.js"></script>	
 	</body>
 </html>
